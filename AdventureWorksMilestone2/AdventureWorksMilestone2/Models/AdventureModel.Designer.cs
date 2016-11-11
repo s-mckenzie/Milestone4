cba2d587d19ddf10514f221270ed5ac54e0101bf
uphayled <1364014@johnabbottcollege.net>
2016-11-11 14:04:06 -0500

    Reviews in the works
    Review Table set up, creating a controller to test some code

--- AdventureWorksMilestone2/AdventureWorksMilestone2/Models/AdventureModel.Designer.cs
+++ AdventureWorksMilestone2/AdventureWorksMilestone2/Models/AdventureModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\1493236\Desktop\Projects\AdventureWorksMilestone2\AdventureWorksMilestone2\Models\AdventureModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\1364014\Desktop\Milestone4\AdventureWorksMilestone2\AdventureWorksMilestone2\Models\AdventureModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
