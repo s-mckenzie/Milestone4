4883938c9a0f279f65161ea18eeb9303851766e6
Matt Buchanan <1449877@johnabbottcollege.net>
2016-11-11 13:27:55 -0500

    Products

---
+++ AdventureWorksMilestone2/AdventureWorksMilestone2/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdventureWorksMilestone2.Models;
+
+namespace AdventureWorksMilestone2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private AdventureWorks2012Entities db = new AdventureWorks2012Entities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = from x in db.Products where x.SellEndDate == null select x;
+            return View(products);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+}
