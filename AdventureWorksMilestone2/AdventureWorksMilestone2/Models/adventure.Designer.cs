d029749b65cb1c8a3bde2edf760ed822fdca0e8f
uphayled <uphayled@gmail.com>
2016-11-25 13:36:26 -0500

    adasdasdasdsadsadacd

--- AdventureWorksMilestone2/AdventureWorksMilestone2/Models/adventure.Designer.cs
+++ AdventureWorksMilestone2/AdventureWorksMilestone2/Models/adventure.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\1449877\Desktop\GitHub\Milestone4\AdventureWorksMilestone2\AdventureWorksMilestone2\Models\Adventure.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\1364014\Desktop\GitHub\Milestone4\AdventureWorksMilestone2\AdventureWorksMilestone2\Models\adventure.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
